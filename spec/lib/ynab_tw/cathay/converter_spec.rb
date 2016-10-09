49f61d0f887f08e130cf02b3e7314cb7048588c3
johnlinvc <johnlinvc@gmail.com>
2016-10-09 19:53:37 +0800

    fix spec

--- spec/lib/ynab_tw/cathay/converter_spec.rb
+++ spec/lib/ynab_tw/cathay/converter_spec.rb
@@ -4,11 +4,11 @@ describe YnabTw::Cathay::Converter do
   describe "#convert" do
     it "can load a specific file" do
       converter = YnabTw::Cathay::Converter.new
-      converter.convert("spec/cathay/withdraw.csv")
+      converter.convert("spec/csv/cathay/withdraw.csv")
     end
     it "can convert it to YNAB compatible csv" do
       converter = YnabTw::Cathay::Converter.new
-      result = converter.convert("spec/cathay/withdraw.csv")
+      result = converter.convert("spec/csv/cathay/withdraw.csv")
       expect(result).not_to be_nil
     end
   end
