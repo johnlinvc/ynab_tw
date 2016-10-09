28379b1d5807d8139aefc28c3ec56503f3833478
johnlinvc <johnlinvc@gmail.com>
2016-10-09 10:52:19 +0800

    able to parse cathay csv

--- lib/ynab_tw/cathay/converter.rb
+++ lib/ynab_tw/cathay/converter.rb
@@ -1,11 +1,25 @@
+require 'csv'
 module YnabTw::Cathay
   class Converter
+
+    def parse(filename)
+      csv = nil
+      File.open(filename, "r:big5:utf-8") do |f|
+        lines = f.each_line.to_a
+        ary = lines.drop(2)
+        headers = %w{date withdraw deposit balance info memo extra}
+        csv_str = ary.join("")
+        csv = ::CSV.parse(csv_str, headers: headers)
+      end
+      return csv
+    end
+
     def convert(filename)
       result = nil
-      File.open(filename, "r:big5:utf-8") do |f|
-        f.each_line{|l| puts(l)}
-      end
+      csv = parse(filename)
+      p csv
       result
     end
+
   end
 end
